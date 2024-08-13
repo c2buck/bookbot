8c2e0dbb07fef34018b60053979980c0d304ee85
github_username_here <email@example.com>
2024-08-13 11:37:21 +1000

    add feature print report v1

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ def main():
     character_count_result = count_characters(text)
     letter_count_result = count_letters(text)
 
-    print(word_count_result) # word count
-    print(character_count_result) # character count
-    print(letter_count_result) #print the count of each letter in dictionary
+    print(f"Total word count: {word_count_result}") #word count
+    print(f"Total character count: {character_count_result}") #character count
+
+    #convert the dictionary to a list of dictionaries
+    letter_count_list = [{"char": char, "count": count} for char, count in letter_count_result.items()]
+
+    #sort the list of dictionaries by the "count" value             
+    letter_count_list.sort(key=lambda x: x["char"])
+
+    for item in letter_count_list:
+        print(f"The '{item['char']}' character was found {item['count']} times")
 
 def get_book_text(path):
     with open(path) as f:
@@ -28,11 +36,12 @@ def count_characters(text):
 def count_letters(text):
     letter_counts = {}
     for char in text:
-        if char.isalpha():
+        if char.isalpha():   
             if char in letter_counts:
                 letter_counts[char] += 1
             else:
                 letter_counts[char] = 1
     return letter_counts
-          
+
+         
 main()
