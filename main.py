6cdb0829ccf9182e4303b75488477647b4694f85
github_username_here <email@example.com>
2024-08-13 10:55:45 +1000

    add feature character count + comments

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    count = word_count(text)
-    print(count)
+
+    word_count_result = word_count(text)
+    character_count_result = count_characters(text)
+
+    print(word_count_result)
+    print(character_count_result)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+# read book frankenstein
     
 def word_count(text):
     words = text.split()
     count = len(words)
     return(count)
+# count words function
 
 def count_characters(text):
-    count_characters = count_characters(text)
-    print(count_characters)
     return len(text)
-           
+# count characters function
+          
 main()
