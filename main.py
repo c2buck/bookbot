a620691ec1a956c2b0b216c3c4acf1820c9af857
github_username_here <email@example.com>
2024-08-13 10:48:59 +1000

    add feature word count

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    count = word_count(text)
+    print(count)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
     
+def word_count(text):
+    words = text.split()
+    count = len(words)
+    return(count)
+
+def count_characters(text):
+    count_characters = count_characters(text)
+    print(count_characters)
+    return len(text)
+           
 main()
