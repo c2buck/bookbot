dee2e371be2d96a42f76179eb710c0f3cc98b582
github_username_here <email@example.com>
2024-08-13 11:15:09 +1000

    add feature count characters

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ def main():
 
     word_count_result = word_count(text)
     character_count_result = count_characters(text)
+    letter_count_result = count_letters(text)
 
-    print(word_count_result)
-    print(character_count_result)
+    print(word_count_result) # word count
+    print(character_count_result) # character count
+    print(letter_count_result) #print the count of each letter in dictionary
 
 def get_book_text(path):
     with open(path) as f:
-        return f.read()
+        return f.read().lower() # convert the text to lowercase
 # read book frankenstein
     
 def word_count(text):
@@ -22,5 +24,15 @@ def word_count(text):
 def count_characters(text):
     return len(text)
 # count characters function
+
+def count_letters(text):
+    letter_counts = {}
+    for char in text:
+        if char.isalpha():
+            if char in letter_counts:
+                letter_counts[char] += 1
+            else:
+                letter_counts[char] = 1
+    return letter_counts
           
 main()
